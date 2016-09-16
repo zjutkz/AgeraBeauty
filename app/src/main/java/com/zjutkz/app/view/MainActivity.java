5b23c3696e01cb5dc6c10577250183530f4cf12b
亢哲 <kangzhe@weidian.com>
2016-09-16 23:25:10 +0800

    use translucent theme in splash page

--- app/src/main/java/com/zjutkz/app/view/MainActivity.java
+++ app/src/main/java/com/zjutkz/app/view/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.GridLayoutManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
